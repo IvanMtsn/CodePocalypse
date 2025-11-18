854fa53975c87f3af4448ad624773025080e5c45
IvanMtsn <ivox300@gmail.com>
2025-11-18 18:07:48 +0100

    [HOT CHANGE] Player Skripte zusammengefasst + enemy layer hinzugefügt
    Ich hab den Begriff erfunden

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     /** TODO: movement to next coordinate, texture scrolling
      * Check if a coordinate point is available in front of the player (raycast w layermask, horizontal offset!!!)
      * scroll texture of tracks if player speed is above a certain value (probably if its just moving anyway?) **/
 
+    [SerializeField] LayerMask _hostileColliders;
     void Start()
     {
         
@@ -13,5 +14,9 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       
     }
 }
