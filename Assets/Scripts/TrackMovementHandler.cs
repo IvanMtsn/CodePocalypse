4d7793ddf654560b7aa2d045895c32b5875a8218
Gnomuran <155955711+Korsten-Thain985@users.noreply.github.com>
2026-02-13 10:18:32 +0100

    "PlayerMovment Audio Fix"

--- Assets/Scripts/TrackMovementHandler.cs
+++ Assets/Scripts/TrackMovementHandler.cs
@@ -4,29 +4,35 @@ public class TrackMovementHandler : MonoBehaviour
 {
     [SerializeField] MeshRenderer _trackMeshRenderer;
     [SerializeField] float _scrollSpeed;
-    
+
     [SerializeField] AudioSource a;
+
+    
     Rigidbody _rb;
+    bool _wasMoving = false;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-          if (!a.isPlaying)
-            {
-                SoundManager.instance.PlayClipOnSource(a, SoundManager.instance.GetPlayPlayerMoveTrack(), 0.6f, true);
-            }
-            else
-            {
-                 if (a.isPlaying)           
-                        a.Pause();
-            }
     }
+
     void Update()
     {
-        if (_rb.linearVelocity.sqrMagnitude > 0.1)
+        bool isMoving = _rb.linearVelocity.sqrMagnitude > 0.1f;
+
+        if (isMoving)
         {
-            _trackMeshRenderer.material.mainTextureOffset = new Vector2(0,_scrollSpeed * Time.realtimeSinceStartup);
+            _trackMeshRenderer.material.mainTextureOffset = new Vector2(0, _scrollSpeed * Time.realtimeSinceStartup);
         }
 
+        if (isMoving != _wasMoving)
+        {
+            if (isMoving)
+                SoundManager.instance.PlayClipOnSource(a, SoundManager.instance.GetPlayPlayerMoveTrack(), 0.6f, true);
+            else
+                a.Pause();
 
+            _wasMoving = isMoving;
+        }
     }
 }
